84f761370721f8cd3b868f24d8fe53668c011575
Raul <raulmayas20@gmail.com>
2021-12-17 13:12:06 -0600

    Updating Settings

--- alien_game.py
+++ alien_game.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """"Overall class to manage game assets and behavior"""
@@ -7,8 +8,10 @@ class AlienInvasion:
     def __init__(self):
         """Run the game, and develop necessary resources"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,650))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion II")
 
     def run_game(self):
@@ -18,6 +21,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redrawing the screen during each loop
+            self.screen.fill(self.settings.bg_color)
             
             # Make the screen visible
             pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Class to store all settings from the Alien Invasion Game"""
+
+    def __init__(self):
+        """Execute game's settings"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 650
+        self.bg_color = (72,61,139)
