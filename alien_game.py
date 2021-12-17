83c357981f548fb9edd0ef0eb381defa52b31f1a
Raul <raulmayas20@gmail.com>
2021-12-17 11:54:45 -0600

    Developing the screen in which the game will run

---
+++ alien_game.py
@@ -0,0 +1,28 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    """"Overall class to manage game assets and behavior"""
+
+    def __init__(self):
+        """Run the game, and develop necessary resources"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion II")
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            # Make the screen visible
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make a game prothotype, and run the game
+    alien_app = AlienInvasion()
+    alien_app.run_game()
