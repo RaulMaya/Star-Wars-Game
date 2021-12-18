d3e423e93d6d0fb325a45ee52d9b801d9aee464e
Raul <raulmayas20@gmail.com>
2021-12-18 14:07:10 -0600

    Event Handlers

--- alien_game.py
+++ alien_game.py
@@ -13,7 +13,7 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
-        pygame.display.set_caption("Alien Invasion II")
+        pygame.display.set_caption("Empire Strikes Back")
 
         self.space_ship = SpaceShip(self)
 
@@ -30,32 +30,40 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key ==pygame.K_RIGHT:
-                        # Move the ship to the right.
-                        self.space_ship.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        # Move the ship to the left.
-                        self.space_ship.moving_left = True
-                    elif event.key == pygame.K_UP:
-                        # Move the ship up.
-                        self.space_ship.moving_up = True
-                    elif event.key == pygame.K_DOWN:
-                        # Move the ship down.
-                        self.space_ship.moving_down = True
-
+                    self._check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
-                    if event.key ==pygame.K_RIGHT:
-                        # Blocking the movement of the ship to the right.
-                        self.space_ship.moving_right = False
-                    elif event.key == pygame.K_LEFT:
-                        # Blocking the movement of the ship to the left.
-                        self.space_ship.moving_left = False
-                    elif event.key == pygame.K_UP:
-                        # Blocking the movement the ship up.
-                        self.space_ship.moving_up = False
-                    elif event.key == pygame.K_DOWN:
-                        # Blocking the movement the ship down.
-                        self.space_ship.moving_down = False
+                    self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Event handler that responds to keypresses"""
+        if event.key ==pygame.K_RIGHT:
+            # Move the ship to the right.
+            self.space_ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            # Move the ship to the left.
+            self.space_ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            # Move the ship up.
+            self.space_ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            # Move the ship down.
+            self.space_ship.moving_down = True
+
+    def _check_keyup_events(self, event):
+        """Event handler that responds to keypresses"""
+        if event.key ==pygame.K_RIGHT:
+            # Blocking the movement of the ship to the right.
+            self.space_ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            # Blocking the movement of the ship to the left.
+            self.space_ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            # Blocking the movement the ship up.
+            self.space_ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            # Blocking the movement the ship down.
+            self.space_ship.moving_down = False
+
 
     def _update_screen(self):
             # Redrawing the screen during each loop
